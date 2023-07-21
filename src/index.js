202722bfedab3c170616d30b50cddf4b6542a745
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-07-21 02:06:58 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -66,15 +66,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj = {pizza} key={pizza.name}/>
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
+
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
@@ -103,7 +109,7 @@ function Pizza(props) {
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 22;
+  const closeHour = 24;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
@@ -112,11 +118,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {" "}
-      {new Date().toLocaleTimeString}. We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
-  return React.createElement("footer", null, "We're currently open!");
+  // return React.createElement("footer", null, "We're currently open!");
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
